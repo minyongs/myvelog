df6b9f16b01e38fd4652ec5feae269d1dfbfc170
Minhyung song <stylish_c@naver.com>
2024-07-06 22:51:03 +0900

    Refactor : 로그아웃시 세션과 쿠키를 삭제하도록 변경

--- src/main/java/com/hellovelog/myvelog/controller/MainController.java
+++ src/main/java/com/hellovelog/myvelog/controller/MainController.java
@@ -1,22 +1,26 @@
 package com.hellovelog.myvelog.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/myvelog")
 public class MainController {
 
-    @GetMapping
-    public String main(){
-
-        return "main";
+    @GetMapping("/myvelog")
+    public String myvelog(Model model, Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            model.addAttribute("loggedIn", false);
+        } else {
+            model.addAttribute("loggedIn", true);
+        }
+        return "main"; // main.html 템플릿을 반환
     }
 
     @GetMapping("/login")
-    public String loginPage(){
-
-        return "login";
+    public String loginPage() {
+        return "login"; // login.html 템플릿을 반환
     }
 }
