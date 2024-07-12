36472b8ccc28d3bd5fa1702f053869e8e2db670c
Minhyung song <stylish_c@naver.com>
2024-07-12 17:17:12 +0900

    .

--- src/main/java/com/hellovelog/myvelog/controller/user/UserController.java
+++ src/main/java/com/hellovelog/myvelog/controller/user/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
-
+//
 @Controller
 @RequestMapping("/myvelog")
 @RequiredArgsConstructor
